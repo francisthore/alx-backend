7e1b4702950cd9034d8a3d3e0cd9a9e407cd9cd1
Francis Thore <thorefrancis@gmail.com>
2024-10-28 23:52:16 +0200

    docstring??

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -41,6 +41,9 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+            retrieves dataset page
+        """
         assert type(page) is int and type(page_size) is int
         assert page > 0 and page_size > 0
 
