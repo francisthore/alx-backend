61af761763c05e2d2165b3992dbdc595fb109191
Francis Thore <thorefrancis@gmail.com>
2024-11-03 23:02:12 +0200

    LRU

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""
+    LRU Cache Module
+"""
+from base_caching import BaseCaching
+from collections import OrderedDict
+
+
+class LRUCache(BaseCaching):
+    """
+        Class that implements LIFO Caching policy
+    """
+    def __init__(self):
+        """
+            Initializes the cache dictionary
+        """
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """
+            Adds an item to the cache dictionary
+        """
+        if key is None or item is None:
+            return
+        if key in self.cache_data:
+            self.cache_data.pop(key)
+
+        elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            old_key, _ = self.cache_data.popitem(last=False)
+            print('DISCARD:', old_key)
+
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """
+            Retrieves an item from the cache dictionary
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        item = self.cache_data.pop(key)
+        self.cache_data[key] = item
+        return item
