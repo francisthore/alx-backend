c39af6f8be1fadaffdf89943826a48c88a4abd62
Francis Thore <thorefrancis@gmail.com>
2024-11-10 22:51:16 +0200

    get_locale

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""
+    Setup module for Babel
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """Configuration stuff here"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """Retrives location from request"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', strict_slashes=False)
+def root():
+    """Entry point to the flask app"""
+    return render_template("1-index.html")
+
+
+if __name__ == '__main__':
+    app.run()
